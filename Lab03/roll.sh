c3b8b327396c3f7f695fa24ec6d0c359e959d3a3
CMartin10016 <martin782@wright.edu>
2026-09-16 18:25:41 +0000

    Step 2: Prompt for number and give random number

--- Lab03/roll.sh
+++ Lab03/roll.sh
@@ -1,3 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "You rolled a ##"
+echo "Enter a number: "
+read num
+
+if [[ $num -lt 1 ]] then
+	echo "ERROR: Number cannot be less than 1"
+else
+	rand_num=$((1 + RANDOM % $num))
+	echo "$rand_num"
+fi
