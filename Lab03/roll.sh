c91ebedcee40ba4cdcf6969e9ef83ef84bfb9227
CMartin10016 <martin782@wright.edu>
2026-09-18 23:03:34 +0000

    Step 4: Command allows both argument and no argument usage

--- Lab03/roll.sh
+++ Lab03/roll.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 if [[ $# -eq 0 ]] then
-	echo "ERROR: No argument given"
-
+	echo "Enter number: "
+	read num
+	if [[ $num -lt 1 ]] then
+		echo "ERROR: Number cannot be less than 1"
+	else
+		rand_num=$((1 + RANDOM % $num))
+		echo "$rand_num"
+	fi
 elif [[ $1 -lt 1 ]] then
 	echo "ERROR: Number cannot be less than 1"
 else
