6fcc37d571f692a834dc8cdbe49f30291b2a93a9
CMartin10016 <martin782@wright.edu>
2026-09-18 23:39:18 +0000

    Part 6: Asks for two arguments determining number of dice and number of faces on each and rolls each die, displaying results

--- Lab03/roll.sh
+++ Lab03/roll.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-if [[ $# -eq 0 || $1 -lt 1 ]] then
-	echo "Enter number: "
-	read num
-	while [[ $num -lt 1 ]] do
-		echo "ERROR: Number cannot be less than 1; enter a new number: "
-		read num
+if [[ $# -eq 0 || $1 -lt 1 || $2 -lt 1 ]] then
+	echo "Enter # of dice rolled: "
+	read dicenum
+	echo "Enter # of faces: "
+	read facenum
+	while [[ $dicenum -lt 1 || $facenum -lt 2 ]] do
+		echo "ERROR: Numbers cannot be less than 1; enter a new numbers: "
+		echo "# of dice rolled: "
+		read dicenum
+		echo "# of faces: "
+		read facenum
 	done
-	rand_num=$((1 + RANDOM % $num))
-	echo "$rand_num"
+	for (( i=1 ; i<=$dicenum; i++ )) do
+		rand_num=$((1 + RANDOM % $facenum))
+		echo "Die $i rolled a $rand_num"
+	done
 else
-	rand_num=$((1 + RANDOM % $1))
-	echo "$rand_num"
+	for (( i=1 ; i<=$1; i++ )) do
+                rand_num=$((1 + RANDOM % $2))
+                echo "Die $i rolled a $rand_num"
+        done
 fi
