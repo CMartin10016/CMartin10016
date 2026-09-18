f4856476f4c4903de83b3eadd4eb37dfcbbdfe8b
CMartin10016 <martin782@wright.edu>
2026-09-18 23:25:04 +0000

    Part 5: While loop asks for number if argument does not exist or is invalid until valid number is given

--- Lab03/roll.sh
+++ Lab03/roll.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-if [[ $# -eq 0 ]] then
+if [[ $# -eq 0 || $1 -lt 1 ]] then
 	echo "Enter number: "
 	read num
-	if [[ $num -lt 1 ]] then
-		echo "ERROR: Number cannot be less than 1"
-	else
-		rand_num=$((1 + RANDOM % $num))
-		echo "$rand_num"
-	fi
-elif [[ $1 -lt 1 ]] then
-	echo "ERROR: Number cannot be less than 1"
+	while [[ $num -lt 1 ]] do
+		echo "ERROR: Number cannot be less than 1; enter a new number: "
+		read num
+	done
+	rand_num=$((1 + RANDOM % $num))
+	echo "$rand_num"
 else
 	rand_num=$((1 + RANDOM % $1))
 	echo "$rand_num"
