f577cb0095a0459b0ca047db8a9e5621e11f8c8a
CMartin10016 <martin782@wright.edu>
2026-09-23 17:40:09 +0000

    Added help command and user guide

--- Lab04/tt.sh
+++ Lab04/tt.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 user-guide () {
-	echo "ADD: bash tt.sh add 'TASK_TO_ADD' OR bash tt.sh add"
+	echo "ADD: bash tt.sh add 'TASK' | bash tt.sh add"
+	echo "Adds given argument to list of tasks, or if no argument is given, asks for input and adds task"
+	echo ""
+	echo "REMOVE: bash tt.sh remove 'TASK' | bash tt.sh remove"
+	echo "Removes given argument or input to list of tasks, or if task does not exist, reports error"
+	echo ""
+	echo "VIEW: bash tt.sh view"
+	echo "Outputs all current tasks"
+	echo ""
+	echo "CLEAR: bassh tt.sh clear"
+	echo "Prompts for confirmation, then clears all current tasks"
 }
 
 case $1 in
@@ -22,7 +32,7 @@ case $1 in
         ;;
 
         help)
-                echo "help commands"
+                user-guide
         ;;
 
 	*)
