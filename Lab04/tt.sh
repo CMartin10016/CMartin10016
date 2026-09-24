7e76070170b2924767508e217f5b2467598a057e
CMartin10016 <martin.782@wright.edu>
2026-09-24 15:05:25 -0400

    Created remove functionality

--- Lab04/tt.sh
+++ Lab04/tt.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
 user-guide () {
-	echo "### Task Tracker Commands:"
-	echo "# ADD: `bash tt.sh add [TASK]` | `bash tt.sh add`"
+	echo "Task Tracker Commands:"
+	echo "ADD: "
+	echo "bash tt.sh add [TASK] | bash tt.sh add"
 	echo "Adds given argument to list of tasks, or if no argument is given, asks for input and adds task."
 	echo ""
-	echo "# REMOVE: `bash tt.sh remove [TASK]` | `bash tt.sh remove`"
+	echo "REMOVE:"
+	echo "bash tt.sh remove [TASK] | bash tt.sh remove"
 	echo "Removes given argument or input to list of tasks, or if task does not exist, reports error."
 	echo ""
-	echo "# VIEW: `bash tt.sh view`"
+	echo "VIEW: "
+	echo "bash tt.sh view"
 	echo "Outputs all current tasks."
 	echo ""
-	echo "# CLEAR: `bash tt.sh clear`"
+	echo "CLEAR: "
+	echo "bash tt.sh clear"
 	echo "Prompts for confirmation, then clears all current tasks."
 }
 
 remove_task () {
-	grep -q "$1" ~/.task) 
-	if [[ $? -eq 0 ]]; then
-		sed -i $1 ~/.task
-	else
-		echo "ERROR: $1 not found within tasks."
-	fi
+        grep -q "$1" ~/.tasks
+        if [[ $? -eq 0 ]]; then
+                sed -i "/$1/d" ~/.tasks
+        else
+                echo "ERROR: $1 not found within tasks."
+        fi
 
 }
 
@@ -53,7 +57,7 @@ case $1 in
         clear)
 		echo "Really delete all tasks? (input y to clear)"
 		read clearinput
-		if [[ clearinput = "y" ]]; then
+		if [[ clearinput -eq "y" ]]; then
 			rm ~/.tasks
 		fi
         ;;
