be6822ac0dab54eee4025fb7a5a07654388d7935
CMartin10016 <martin.782@wright.edu>
2026-09-24 14:40:03 -0400

    Updated user-guide to use markdown

--- Lab04/tt.sh
+++ Lab04/tt.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 user-guide () {
-	echo "ADD: bash tt.sh add 'TASK' | bash tt.sh add"
-	echo "Adds given argument to list of tasks, or if no argument is given, asks for input and adds task"
+	echo "### Task Tracker Commands:"
+	echo "# ADD: `bash tt.sh add [TASK]` | `bash tt.sh add`"
+	echo "Adds given argument to list of tasks, or if no argument is given, asks for input and adds task."
 	echo ""
-	echo "REMOVE: bash tt.sh remove 'TASK' | bash tt.sh remove"
-	echo "Removes given argument or input to list of tasks, or if task does not exist, reports error"
+	echo "# REMOVE: `bash tt.sh remove [TASK]` | `bash tt.sh remove`"
+	echo "Removes given argument or input to list of tasks, or if task does not exist, reports error."
 	echo ""
-	echo "VIEW: bash tt.sh view"
-	echo "Outputs all current tasks"
+	echo "# VIEW: `bash tt.sh view`"
+	echo "Outputs all current tasks."
 	echo ""
-	echo "CLEAR: bassh tt.sh clear"
-	echo "Prompts for confirmation, then clears all current tasks"
+	echo "# CLEAR: `bash tt.sh clear`"
+	echo "Prompts for confirmation, then clears all current tasks."
 }
 
 remove_task () {
